a5572b086f8b5f7bb6f34d1472e1d6f58e6da2a0
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-07-28 15:29:37 +0900

    API通信してリスト取得のメソッド修正
    [SerializeField]でオブジェクトを渡していた所を修正して子のオブジェクトを取得して扱うように変更。
    API通信をしてデータを受け取るメソッドと、そのデータをテキストオブジェクトに渡すメソッドに機能を分けた。

--- Script/ChatRoomListGet.cs
+++ Script/ChatRoomListGet.cs
@@ -12,18 +12,19 @@ public class ChatRoomListGet : MonoBehaviour
     //接続するURL
     private const string URL = "http://192.168.56.56/api/getAllChatRoom/";
 
-    //オブジェクト関係
+    //オブジェクト関係  objectを探すようにする。
+    /*
     [SerializeField]
     public Text chatRoomText;
-
-    void Start()
+    */
+    async Task Start()
     {
         Debug.Log("Start開始");
-        GetChatRommList(URL);
+        await TextDisplay();
         Debug.Log("Start終了");
     }
 
-    public async Task GetChatRommList(string url)
+    public async Task<ChatList> GetChatRommList(string url)
     {
         Debug.Log("GetChatRoomList開始");
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
@@ -33,18 +34,35 @@ public class ChatRoomListGet : MonoBehaviour
             {
                 //通信失敗
                 Debug.Log(webRequest.error);
+                Debug.Log("GetChatRoomList終了");
+                return null;
             }
             else
             {
                 Debug.Log("text:" + webRequest.downloadHandler.text);
                 //通信成功
                 var response = JsonUtility.FromJson<ChatList>("{\"chatLists\":" + webRequest.downloadHandler.text + "}");
-                foreach (ChatListResult clr in response.chatLists)
-                {
-                    chatRoomText.text += string.Format("({0}){1}:{2}\n", clr.id, clr.chat_room_name, clr.created_at);
-                }
+                Debug.Log("GetChatRoomList終了");
+                return response;
             }
         }
-        Debug.Log("GetChatRoomList終了");
+    }
+
+    public async Task TextDisplay()
+    {
+        Debug.Log("TextDisplay開始");
+        Text text;
+        GameObject obj = transform.Find("ChatRoomListText").gameObject;
+        text = obj.GetComponent<Text>();
+        text.text = "";
+
+        var request = await GetChatRommList(URL);
+        foreach (ChatListResult clr in request.chatLists)
+        {
+            text.text += string.Format("({0}){1}:{2}\n", clr.id, clr.chat_room_name, clr.created_at);
+        }
+
+        Debug.Log(request.chatLists[0].chat_room_name);
+        Debug.Log("TextDisplay終了");
     }
 }
